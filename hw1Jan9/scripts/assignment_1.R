1bf35d355e81b9aa19fe96078bcde8b8f7a68339
Jonathan Dushoff <dushoff@mcmaster.ca>
2026-01-14 12:10:32 -0500

    comments from JD

--- hw1Jan9/scripts/assignment_1.R
+++ hw1Jan9/scripts/assignment_1.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 
 ## JD: Moved your directory to a simpler, space-free name, sorry.
+## JD: The way to confirm you are running this from the assignment directory is that the data path is relative to that (i.e., starts from there).
+
 switching_data <- read_csv("hw1Jan9/data/raw/WL_switching_df.csv") #Data is from the previous PhD student who worked in Reuven's lab. 
 wl_switch <- select(switching_data, participant.ID, treatment, switch.pref) #selecting only the appropriate columns necessary for the analysis
 wl_switch <- wl_switch %>% #creating a new column that assigns the likert scale values of switching preferece to a binary value for analysis
