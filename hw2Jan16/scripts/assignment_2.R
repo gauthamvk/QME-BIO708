07977fdbf9be9309001dfed04c01a9b955569e67
Ben Bolker <bbolker@gmail.com>
2026-01-19 13:55:54 -0500

    HW 2 comments

--- hw2Jan16/scripts/assignment_2.R
+++ hw2Jan16/scripts/assignment_2.R
@@ -1,28 +1,40 @@
 library(tidyverse)
 
-switching_data <- read_csv("hw1 - jan. 9/data/raw/WL_switching_df.csv")
+## BMB: this is not what you called the data file ...
+## switching_data <- read_csv("hw1 - jan. 9/data/raw/WL_switching_df.csv")
+switching_data <- read_csv("hw1Jan9/data/raw/WL_switching_df.csv")
 
 #Examining the structure of the data 
 glimpse(switching_data)#examines the structure of each column - was told it is a better str()
 
 #Examining data for problems
-colSums(is.na(switching_data)) #colSums(is.na(dataframe)) shows you the missing values from each column - StackOverflow 
+colSums(is.na(switching_data)) #colSums(is.na(dataframe)) shows you the missing values from each column - StackOverflow
+## BMB: consider
+stopifnot(!any(is.na(switching_data)))
+
 count(switching_data, treatment, sort = TRUE) #counts number of data points in each treatment 
-count(switching_data, switch.pref) #swithcing preference is in a likert scale. However, the numeric value would need to be change to be a categorical value to be either switching or no switching
+count(switching_data, switch.pref) #switching preference is in a likert scale. However, the numeric value would need to be change to be a categorical value to be either switching or no switching
 
 #Histogram plot for checking if there are any values that fall outside of the likert scale 
 plot_one <- ggplot(switching_data, aes(x=switch.pref))+geom_histogram(binwidth = 1)
 plot_one
+## BMB: how about
+stopifnot(all(between(switching_data$switch.pref, 1, 5)))
+## ??
 
 #Scatter plot to check if baseline positive mood and post positive mood seem accurate 
-plot_two <- ggplot(switching_data, aes(x = pos.baseline, y = pos.post)) + geom_point()
+plot_two <- ggplot(switching_data, aes(x = pos.baseline, y = pos.post)) + stat_sum() ## BMB: stat_sum to identify repeated points
 plot_two + theme(legend.position = "none")
 
 #Fixing problems and restructuring so it is ready for analysis
 switching_data <- (switching_data |> mutate(treatment = as.factor(treatment), gender = as.factor(gender)))
+## BMB: can use across() here
  
-#saving file as RDS
+##saving file as RDS
+## BMB: fails if dir doesn't already exist
+ddir <- "hw2Jan16/data/clean"
+suppressWarnings(dir.create(ddir, recursive = TRUE))
 saveRDS(switching_data,
-        file = ("hw2Jan16/data/clean/wl_switching_clean.rds"))
+        file = file.path(ddir, "wl_switching_clean.rds"))
                    
         
--- hw2Jan16/scripts/assignment_2_rdsPlot.R
+++ hw2Jan16/scripts/assignment_2_rdsPlot.R
@@ -4,5 +4,21 @@ clean_switching_data <- readRDS("hw2Jan16/data/clean/wl_switching_clean.rds")
 clean_switching_data
 
 wl_plot <- ggplot(clean_switching_data, aes(x = treatment, y = switch.pref)) + #did a box plot as it helps me visualize what the pattern in preferences to switch were depending on the treatment
-  geom_boxplot(alpha = 0.25) + geom_jitter(aes(color = treatment))
+    geom_boxplot(alpha = 0.25) + geom_jitter(aes(color = treatment))
+                                             height=0, width = 0.25)
 wl_plot + theme(legend.position = "none")
+
+## BMB: stat_sum or geom_beeswarm might be nicer? jittering with height>0
+## is probably unwanted here
+
+gg0 <- ggplot(clean_switching_data, aes(x = treatment, y = switch.pref,
+                                        color = treatment))
+
+gg0 + stat_sum()
+library(ggbeeswarm)
+gg0 + geom_beeswarm()
+
+## mark: 2
+
+
+
